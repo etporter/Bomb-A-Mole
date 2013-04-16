658c16e898278b9288442faefe3207888962c03c
Ethan Porter <Ethan@ethan-porter.wireless.um.maine.edu>
2013-04-16 10:29:18 -0400

    Added basic bomb placing functionality

--- Bomb-A-Mole.py
+++ Bomb-A-Mole.py
@@ -16,7 +16,7 @@ import math
 
 class cell:
 	def __init__(self,type,xCoord,yCoord):
-	
+		
 		self.carrot = pygame.image.load('carrot.png')
 		self.cabbage = pygame.image.load('cabbage.png')
 		self.bomb = pygame.image.load('bomb.png')
@@ -42,6 +42,21 @@ class cell:
 		
 		self.disp = self.image.get_rect(center = (self.x,self.y))
 		
+	def placeBomb(self):
+# 		print 'placeBomb activated!'
+		self.mouseLoc = pygame.mouse.get_pos()
+		self.mouseX = self.mouseLoc[0]
+		self.mouseY = self.mouseLoc[1]
+		if abs(self.x - self.mouseX) <= 40 and abs(self.y - self.mouseY) <= 40:
+# 			print 'placeBomb if sequence activated!'
+			self.kind = 3
+			self.image = self.bomb
+	
+# 	def clickOn(self):
+# 		for event in pygame.event.get():
+# 			if event.type == MOUSEBUTTONDOWN:
+# 				print 'Mouse clicked!'
+
 # 	def action:
 # 		when mouse clicks:
 # 			if mouseX is inside range (xMax-xMin) and mouseY is inside range (yMax-yMin):
@@ -112,8 +127,6 @@ class game:
 		pygame.init()
 		self.size = width, height = 1200, 650
 		self.screen = pygame.display.set_mode(self.size)
-		
-# 		self.cells = [cell(1,80,80),cell(1,161,80),cell(1,242,80),cell(1,323,80),cell(1,404,80),cell(1,485,80),cell(1,80,161),cell(1,161,161),cell(1,242,161),cell(1,323,161),cell(1,404,161),cell(1,485,161),cell(1,80,242),cell(1,161,242),cell(1,242,242),cell(1,323,242),cell(1,404,242),cell(1,485,242),cell(1,80,323),cell(1,161,323),cell(1,242,323),cell(1,323,323),cell(1,404,323),cell(1,485,323),cell(1,80,404),cell(1,161,404),cell(1,242,404),cell(1,323,404),cell(1,404,404),cell(1,485,404),cell(1,80,485),cell(1,161,485),cell(1,242,485),cell(1,323,485),cell(1,404,485),cell(1,485,485)]
 
 		self.cells = [cell(1,120,120),cell(1,201,120),cell(1,282,120),cell(1,363,120),cell(1,444,120),cell(1,525,120),cell(1,120,201),cell(1,201,201),cell(1,282,201),cell(1,363,201),cell(1,444,201),cell(1,525,201),cell(1,120,282),cell(1,201,282),cell(1,282,282),cell(1,363,282),cell(1,444,282),cell(1,525,282),cell(1,120,363),cell(1,201,363),cell(1,282,363),cell(1,363,363),cell(1,444,363),cell(1,525,363),cell(1,120,444),cell(1,201,444),cell(1,282,444),cell(1,363,444),cell(1,444,444),cell(1,525,444),cell(1,120,525),cell(1,201,525),cell(1,282,525),cell(1,363,525),cell(1,444,525),cell(1,525,525)]
 		
@@ -122,6 +135,10 @@ class game:
 				if event.type == pygame.QUIT:
 					pygame.quit()
 					sys.exit()
+				if event.type == MOUSEBUTTONDOWN:
+# 					print 'Mouse clicked!'
+					for i in self.cells:
+						cell.placeBomb(i)
 			for i in self.cells:
 				self.screen.blit(i.image,i.disp)
 				pygame.display.flip()
