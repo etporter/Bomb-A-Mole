5a6f57bdb4b973924fd7b8bc3e67d60cb7677b8e
Ethan Porter <ethan.porter@maine.edu>
2013-04-29 23:33:25 -0400

    High score

---
+++ high score.py
@@ -0,0 +1,40 @@
+import sys, pygame
+ 
+ 
+pygame.init()
+  
+
+pygame.display.set_caption('High Score')
+
+dimensions=[1200,650]
+
+font = pygame.font.Font(None, 48)
+
+screen=pygame.display.set_mode(dimensions)
+
+
+
+file = open("High Score.txt", "r") 
+# line = file.readline()
+text = file.read() 
+
+# print text
+
+# while line: 
+# 	print line 
+# 	line = file.readline()
+
+
+text=font.render(text, True, [255,255,255])
+screen.blit(text, [100, 200]) 
+
+while 1:
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			pygame.quit()
+			sys.exit()
+
+	pygame.display.flip()
+
+
+
